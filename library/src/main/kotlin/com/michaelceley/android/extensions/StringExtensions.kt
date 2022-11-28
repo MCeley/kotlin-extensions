6161286b33009a2d4fd036311f3e1a40335e34cf
Michael Celey <michael.celey@heapanalytics.com>
2022-11-27 22:16:30 -0500

    Adding in extension for truncating string length while respecting UTF-16 code point boundaries.

--- library/src/main/kotlin/com/michaelceley/android/extensions/StringExtensions.kt
+++ library/src/main/kotlin/com/michaelceley/android/extensions/StringExtensions.kt
@@ -1,2 +1,24 @@
 package com.michaelceley.android.extensions
 
+fun String.utf16TruncateLength(maxLength: Int) : Pair<String, Boolean> {
+
+    // If the value of this is under maxLength units, return unchanged value.
+    if(length <= maxLength) {
+        return this to false
+    }
+
+    // Go to the last valid index and add one code point to it.
+    val indexAfterLastValidCodePoint = this.offsetByCodePoints(maxLength - 1, 1)
+
+    // If adding one code point to the last valid index jumps the index by two
+    // or more, it's a multi-unit character and we should drop the partial character
+    // and truncate to (maxLength - 1).
+    val truncatedValue = if(indexAfterLastValidCodePoint >= maxLength + 1) {
+        this.substring(0, maxLength - 1)
+    } else {
+        this.substring(0, maxLength)
+    }
+
+    // Return the truncated value and true to denote the value has been truncated.
+    return truncatedValue to true
+}
--- library/src/test/kotlin/com/michaelceley/android/extensions/ExtensionTest.kt
+++ library/src/test/kotlin/com/michaelceley/android/extensions/ExtensionTest.kt
@@ -6,4 +6,40 @@ import org.junit.Assert.*
 
 class ExtensionTest {
 
+    @Test
+    fun testTruncateDoesNotChangeValueIfLengthIsMax() {
+        // Using a non-ascii character that is one code point in length for testing.
+        val expected = "あ".repeat(20)
+        val truncated = expected.utf16TruncateLength(20)
+
+        assertEquals(expected, truncated.first)
+    }
+
+    @Test
+    fun testTruncateDoesNotChangeValueIfLengthIsLessThanMax() {
+        val expected = "あ".repeat(18)
+        val truncated = expected.utf16TruncateLength(20)
+
+        assertEquals(expected, truncated.first)
+    }
+
+    @Test
+    fun testTruncateShortensStringToMaxLengthIfNoSurrogatePair() {
+        val expected = "あ".repeat(20)
+        val testValue = "あ".repeat(30)
+        val truncated = testValue.utf16TruncateLength(20)
+
+        assertEquals(expected, truncated.first)
+    }
+
+    @Test
+    fun testTruncateProperlyRemovesSurrogatePairIfOnTruncationBoundary() {
+        val expected = "あ".repeat(19)
+        val testValue = "あ".repeat(19) + "\uD83D\uDC68"
+
+        // Try to cut in the middle of the surrogate pair.
+        val truncated = testValue.utf16TruncateLength(20)
+
+        assertEquals(expected, truncated.first)
+    }
 }
